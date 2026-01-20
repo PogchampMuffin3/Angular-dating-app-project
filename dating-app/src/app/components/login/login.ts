609aaff262a6ecd48c7120fcc9c6f11a66e3ca0a
jakubdabrowskii <jakubdabrowski818@gmail.com>
2026-01-20 19:21:15 +0100

    redesign app login

--- dating-app/src/app/components/login/login.ts
+++ dating-app/src/app/components/login/login.ts
@@ -1,36 +1,29 @@
-import {Component, inject, ChangeDetectorRef} from '@angular/core';
-import {Router, RouterLink} from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
 import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink, FormsModule, CommonModule],
+  standalone: true,
+  imports: [FormsModule, RouterLink],
   templateUrl: './login.html',
-  styles: ``,
+  styles: ``
 })
 export class Login {
-  loginData = { email:'', password: ''};
-  errorMessage = '';
+  // ZMIANA: Dodanie brakujących właściwości
+  email = '';
+  password = '';
 
   private authService = inject(Auth);
   private router = inject(Router);
-  private cdr = inject(ChangeDetectorRef);
 
-  onLogin(){
-    this.errorMessage = '';
-    this.authService.login(this.loginData.email, this.loginData.password).subscribe({
-      next: (response) => {
-        const user = this.authService.getCurrentUserValue();
-        if(user){
-          this.router.navigate(['/feed']);
-        }
-      },
-      error: (err) => {
-        this.errorMessage = 'Błędne dane';
-        this.cdr.detectChanges();
-      }
-    });
+  // ZMIANA: Zmiana nazwy metody na onLogin (albo możesz zostawić login, ale wtedy zmień w HTML)
+  onLogin() {
+    if (this.authService.login(this.email, this.password)) {
+      this.router.navigate(['/feed']);
+    } else {
+      alert('Invalid credentials');
+    }
   }
 }
